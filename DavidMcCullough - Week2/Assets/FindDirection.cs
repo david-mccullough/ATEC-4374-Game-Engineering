51d69f5397e1904fe2538c8c928333296b40b73a
david mccullough <dwm140030@utdallas.edu>
2017-01-17 17:44:41 -0600

    Initial week 2 assignment commit.

---
+++ DavidMcCullough - Week2/Assets/FindDirection.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class FindDirection : MonoBehaviour {
+
+	public Transform target;
+	public float offsetLength = 1f;
+	public Color color = Color.cyan;
+	public float duration;
+
+	// Update is called once per frame
+	void Update () {
+
+		if (target == null)
+			return;
+
+		Vector3 targetPosition = target.position;
+		Vector3 selfPosition = transform.position;
+		//transform.position is shorthand for GetComponent<Transfrom>.position
+
+		Vector3 relativeOffset = targetPosition - selfPosition;
+
+		Vector3 relativeDirection = relativeOffset.normalized * offsetLength;
+
+		Debug.DrawRay (selfPosition, relativeDirection, color, duration, true); 
+		Debug.DrawRay (selfPosition + relativeDirection, Vector3.up, color, duration, false);
+		//Debug.DrawRay (target.position, relativeOffset, color, duration); 
+	}
+}
