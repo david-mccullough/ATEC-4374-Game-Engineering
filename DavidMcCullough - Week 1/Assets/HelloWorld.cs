6ec02ad95bf1f167d7506abbb9c7a5e3c06342b6
david mccullough <dwm140030@utdallas.edu>
2017-01-17 16:31:19 -0600

    Initial project commit.

---
+++ DavidMcCullough - Week 1/Assets/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	public string broadcastText = "Default message";
+
+	// Use this for initialization
+	void Start () {
+		print (broadcastText);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ DavidMcCullough - Week 1/Assets/Mover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(Input.GetButtonDown("Up"))
+		{
+			transform.position += Vector3.up;
+		}
+		if(Input.GetButtonDown("Down"))
+		{
+			transform.position += Vector3.down;
+		}
+		if(Input.GetButtonDown("Left"))
+		{
+			transform.position += Vector3.left;
+		}
+		if(Input.GetButtonDown("Right"))
+		{
+			transform.position += Vector3.right;
+		}
+	}
+}
