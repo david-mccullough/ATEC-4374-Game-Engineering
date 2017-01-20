e3c1da00f28e043125b4807ab840cbe95e33ffb3
David - HomePC <dwm140030@utdallas.edu>
2017-01-20 16:03:14 -0600

    Part one and part two of assignment 2 complete.

---
+++ DavidMcCullough - Week2/Assets/AccelerateChaser.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class AccelerateChaser : MonoBehaviour {
+
+	public Transform target;
+	public float speed = .5f;
+	public Vector3 velocity = new Vector3(0f, 0f, 0f);
+	public float acceleration = 1f;
+	public float maximumVelocity = .15f;
+
+	public float offsetLength = 1f;
+	public Color color = Color.cyan;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target == null)
+			return;
+
+		Vector3 targetPosition = target.position;
+		Vector3 selfPosition = transform.position;
+
+		Vector3 relativeOffset = targetPosition - selfPosition;
+		// Finding direction from self to target position
+		Vector3 relativeDirection = relativeOffset.normalized;
+		//Draw relative direction at self
+		Debug.DrawRay (selfPosition, relativeDirection * offsetLength, color);
+
+		velocity += relativeDirection * (speed * Time.deltaTime);
+
+		//clamp velocity to maximumVelocity
+		velocity = Vector3.ClampMagnitude(velocity, maximumVelocity);
+
+		//Move self toward target at accelerating velocity
+		Vector3 newPosition = selfPosition + velocity;
+		transform.position = newPosition;
+	}
+}
---
+++ DavidMcCullough - Week2/Assets/FixedChaser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class FixedChaser : MonoBehaviour {
+
+	public Transform target;
+	public float speed = 5f;
+	public float offsetLength = 1f;
+	public Color color = Color.cyan;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (target == null)
+			return;
+
+		Vector3 targetPosition = target.position;
+		Vector3 selfPosition = transform.position;
+
+		Vector3 relativeOffset = targetPosition - selfPosition;
+		// Finding direction from self to target position
+		Vector3 relativeDirection = relativeOffset.normalized;
+		//Draw relative direction at self
+		Debug.DrawRay (selfPosition, relativeDirection * offsetLength, color);
+
+		Vector3 velocity = relativeDirection * (speed * Time.deltaTime);
+
+		//Move self toward target at fixed velocity if out of range
+		if (relativeOffset.magnitude > .1f)
+		{
+			Vector3 newPosition = selfPosition + velocity;
+			transform.position = newPosition;
+		}
+	}
+}
