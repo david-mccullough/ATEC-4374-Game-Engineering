6b8ba66d8752d4816c1a17137434ee42194071d2
david mccullough <dwm140030@utdallas.edu>
2017-01-24 18:11:23 -0600

    Week 3 project files committed. Scene set up with gravitational force on sphere.

---
+++ DavidMcCullough - Week3/Assets/BallBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BallBehavior : MonoBehaviour {
+
+	public float gravity = .5f;
+	public Vector3 velocity = new Vector3(0f, 0f, 0f);
+	public float maximumVelocity = .15f;
+
+	public float tailDuration = 5f;
+	public Color color = Color.cyan;
+
+	void Update () 
+	{
+		Vector3 selfPosition = transform.position;
+
+		//Add gravity to acceleration
+		velocity += Vector3.down * (gravity * Time.deltaTime);
+
+		RaycastHit hit;
+		if (Physics.SphereCast(selfPosition, 1f, velocity, out hit, velocity.magnitude))
+		{
+			velocity += Vector3.Reflect (velocity, hit.normal);
+		}
+
+		//clamp velocity to maximumVelocity
+		velocity = Vector3.ClampMagnitude(velocity, maximumVelocity);
+
+		//Move self toward target at accelerating velocity
+		Vector3 newPosition = selfPosition + velocity;
+		transform.position = newPosition;
+
+		//Draw motion trail
+		Debug.DrawRay (selfPosition, Vector3.one, color, tailDuration, true);
+	}
+}
---
+++ DavidMcCullough - Week3/Assets/GroupMove.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class GroupMove : MonoBehaviour {
+
+	public float selectRadius = 3f;
+	LayerMask mask;
+
+	Collider[] currentSelection;
+	void Update ()
+	{
+		if (currentSelection == null) {
+			SelectOnClick ();
+		} else {
+			MoveOnClick ();
+		}
+	}
+
+	void SelectOnClick()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 10000f))
+			{
+				SelectInArea(hit.point);
+			}
+		}
+	}
+
+	void SelectInArea(Vector3 selectionPosition)
+	{
+		currentSelection = Physics.OverlapSphere (selectionPosition, selectRadius);
+
+		if (currentSelection.Length == 0)
+			return;
+
+		foreach (Collider hit in currentSelection)
+		{
+			print (hit.name);
+		}
+	}
+
+	void MoveOnClick()
+	{
+			//wow
+	}
+
+	void MoveToPosition(Vector3 movePos)
+	{
+		foreach (Collider collider in currentSelection)
+		{
+			collider.transform.position = movePos;
+		 }
+	}
+}
