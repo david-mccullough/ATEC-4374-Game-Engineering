b2e2550e841c1531c6e7a3bc186fe1f7fd873b95
david mccullough <dwm140030@utdallas.edu>
2017-01-31 18:00:17 -0600

    Changes made in class to Week3.

--- DavidMcCullough - Week3/Assets/BallBehavior.cs
+++ DavidMcCullough - Week3/Assets/BallBehavior.cs
@@ -13,23 +13,34 @@ public class BallBehavior : MonoBehaviour {
 	void Update () 
 	{
 		Vector3 selfPosition = transform.position;
-
+		Vector3 travelDir = velocity.normalized;
+		float travelDist = velocity.magnitude;
 
 		//Add gravity to acceleration
 		velocity += Vector3.down * (gravity * Time.deltaTime);
 
-		RaycastHit hit;
-		if (Physics.SphereCast(selfPosition, radius, velocity.normalized, out hit, velocity.magnitude)) 
+		int collisionCount = 3;
+		while (collisionCount > 0)
 		{
-			velocity = Vector3.Reflect (velocity, hit.normal);
-			Debug.Log ("Hit surface");
+			RaycastHit hit;
+			if (Physics.SphereCast (selfPosition, radius, velocity.normalized, out hit, travelDist)) {
+				selfPosition += travelDir * (hit.distance - 0.1f);
+				velocity = Vector3.Reflect (velocity, hit.normal);
+				Debug.Log ("Hit surface");
+			}
+			else
+			{
+				selfPosition += travelDir * travelDist;
+				break;
+			}
+			collisionCount--;
 		}
 
 		//clamp velocity to maximumVelocity
 		velocity = Vector3.ClampMagnitude(velocity, maximumVelocity);
 
 		//Move self toward target at accelerating velocity
-		Vector3 newPosition = selfPosition + velocity;
+		Vector3 newPosition = selfPosition;
 		transform.position = newPosition;
 
 		//Draw motion trail
